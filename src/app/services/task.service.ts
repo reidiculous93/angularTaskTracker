817da5d553b959fb45049379eab05368793e74c5
Reid Vrana <reidvrana93@gmail.com>
2021-07-15 14:33:01 -0500

    working add and remove tasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -5,7 +5,7 @@ import { Task } from '../Task';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/json';
+    'Content-Type': 'application/json'
   })
 }
 
@@ -29,5 +29,9 @@ export class TaskService {
   updateTaskReminder(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.put<Task>(url, task, httpOptions);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
   }
 }
